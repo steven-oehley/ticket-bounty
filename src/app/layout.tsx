90c19faf9829103d29d44f773749f3ec19440673
Steve <steve.gitprofile@outlook.com>
2025-01-27 07:54:31 +0200

    fixed import order in layout to rectify build isssue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
-import "./globals.css";
-
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import Header from "@/components/Header";
 import { ThemeProvider } from "@/components/theme/ThemeProvider";
+
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
