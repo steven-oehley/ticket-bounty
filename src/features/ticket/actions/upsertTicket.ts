f090affd9d0b36802a08b2271add07795edc4457
Steve <steve.gitprofile@outlook.com>
2025-02-08 09:01:32 +0200

    used useAction to return an error message for form

--- src/features/ticket/actions/upsertTicket.ts
+++ src/features/ticket/actions/upsertTicket.ts
@@ -2,6 +2,7 @@
 
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { z } from "zod";
 
 import { ticketsPath } from "@/constants/paths";
 import { prisma } from "@/lib/prisma";
@@ -10,21 +11,33 @@ interface TActionState {
   message: string;
 }
 
+const upsertTicketSchema = z.object({
+  title: z.string().nonempty().min(3).max(191),
+  content: z.string().nonempty().min(10).max(1024),
+});
+
 export const upsertTicket = async (
   ticketId: string | undefined,
   _actionState: TActionState,
   formData: FormData
 ) => {
-  const data = {
-    title: formData.get("title") as string,
-    content: formData.get("content") as string,
-  };
+  try {
+    // validation errors can occur here
+    // need to handle them
+    const data = upsertTicketSchema.parse({
+      title: formData.get("title"),
+      content: formData.get("content"),
+    });
 
-  await prisma.ticket.upsert({
-    where: { id: ticketId || "" },
-    update: data,
-    create: data,
-  });
+    await prisma.ticket.upsert({
+      where: { id: ticketId || "" },
+      update: data,
+      create: data,
+    });
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (e) {
+    return { message: "Failed to create ticket!" };
+  }
 
   revalidatePath(ticketsPath);
 
