9da13265e97b125732128fbc9d74fa4e9789a615
Steve <steve.gitprofile@outlook.com>
2025-04-20 10:15:20 +0200

    added validations and prevented reset by passing back payload

--- src/features/ticket/actions/upsert-ticket.ts
+++ src/features/ticket/actions/upsert-ticket.ts
@@ -3,6 +3,8 @@
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+import { z } from 'zod';
+
 import { ticketsPath } from '@/constants/paths';
 import { prisma } from '@/lib/prisma';
 
@@ -10,24 +12,42 @@ import { prisma } from '@/lib/prisma';
 // If the record already exists in the database, it updates that record with new values
 // If the record doesn't exist yet, it inserts a new record
 
+const upsertTicketSchema = z.object({
+  content: z
+    .string()
+    .min(3, 'Content is required')
+    .max(191, 'Content is too long'),
+  title: z.string().min(15, 'Title is required').max(1024, 'Title is too long'),
+});
+
 export const upserticket = async (
   ticketId: string | undefined,
   // when using actionState, we need to pass in the state object
-  actionState: { message: string },
+  actionState: { message: string; payload?: FormData },
   formData: FormData,
 ) => {
-  const data = {
-    content: formData.get('content') as string,
-    title: formData.get('title') as string,
-  };
+  try {
+    // try and validate
+    const data = upsertTicketSchema.parse({
+      content: formData.get('content'),
+      title: formData.get('title'),
+    });
 
-  await prisma.ticket.upsert({
-    create: data,
-    update: data,
-    where: {
-      id: ticketId || '',
-    },
-  });
+    // try and upsert
+    await prisma.ticket.upsert({
+      create: data,
+      update: data,
+      where: {
+        id: ticketId || '',
+      },
+    });
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    return {
+      message: 'Something went wrong',
+      payload: formData,
+    };
+  }
 
   // Revalidate the tickets path to ensure the latest data is fetched for both cases
   revalidatePath(ticketsPath);
--- src/features/ticket/components/ticket-upsert-form.tsx
+++ src/features/ticket/components/ticket-upsert-form.tsx
@@ -18,7 +18,7 @@ const TicketUpsertForm = ({ ticket }: TicketUpdateFormProps) => {
   // allows us to attach state to the action we pass in
   // also requires initial state to be passed in
   // returns an enhanced action function and a state object
-  const [actionsState, action] = useActionState(
+  const [actionState, action] = useActionState(
     upserticket.bind(null, ticket?.id),
     {
       message: '',
@@ -27,11 +27,24 @@ const TicketUpsertForm = ({ ticket }: TicketUpdateFormProps) => {
   return (
     <form action={action} className="flex flex-col gap-y-3">
       <Label htmlFor="title">Title</Label>
-      <Input defaultValue={ticket?.title} id="title" name="title" type="text" />
+      <Input
+        defaultValue={
+          (actionState.payload?.get('title') as string) ?? ticket?.title
+        }
+        id="title"
+        name="title"
+        type="text"
+      />
       <Label htmlFor="content">Content</Label>
-      <Textarea defaultValue={ticket?.content} id="content" name="content" />
+      <Textarea
+        defaultValue={
+          (actionState.payload?.get('content') as string) ?? ticket?.content
+        }
+        id="content"
+        name="content"
+      />
       <SubmitBtn label={ticket ? 'Update' : 'Create'} />
-      <span>{actionsState.message}</span>
+      <span>{actionState.message}</span>
     </form>
   );
 };
