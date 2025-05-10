8ef86d6bce242247a41d03ca4f1e03ce84c2e4c5
Steve <steve.gitprofile@outlook.com>
2025-05-10 18:13:25 +0200

    added getAuth function

---
+++ src/features/auth/queries/get-auth.ts
@@ -0,0 +1,41 @@
+import { cache } from 'react';
+import { cookies } from 'next/headers';
+
+import { lucia } from '@/lib/lucia';
+
+export const getAuth = cache(async () => {
+  const sessionId =
+    (await cookies()).get(lucia.sessionCookieName)?.value ?? null;
+
+  if (!sessionId) {
+    return {
+      session: null,
+      user: null,
+    };
+  }
+
+  const result = await lucia.validateSession(sessionId);
+
+  try {
+    if (result.session && result.session.fresh) {
+      const sessionCookie = lucia.createSessionCookie(result.session.id);
+      (await cookies()).set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes,
+      );
+    }
+    if (!result.session) {
+      const sessionCookie = lucia.createBlankSessionCookie();
+      (await cookies()).set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes,
+      );
+    }
+  } catch {
+    // do nothing if used in a RSC
+  }
+
+  return result;
+});
